2b3cc694fcec382f62558c3e16612082fc830065
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2025-06-04 16:50:49 -0400

    revert

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 data "proxmox_virtual_environment_nodes" "available_nodes" {}
-data "proxmox_virtual_environment_time" "first_node_time" {
-  node_name = data.proxmox_virtual_environment_nodes.available_nodes.names[0]
-}
 
 output "nodes" {
   value = data.proxmox_virtual_environment_nodes.available_nodes
-}
-
-output "time" {
-  value = data.proxmox_virtual_environment_time.first_node_time
 }
