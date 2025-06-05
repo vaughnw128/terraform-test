f4a7fdd52ce242c64a2ee84e006e25e8d53409ec
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2025-06-05 10:04:27 -0400

    Add twice cooked pork

--- main.tf
+++ main.tf
@@ -3,6 +3,17 @@ data "proxmox_virtual_environment_time" "first_node_time" {
   node_name = data.proxmox_virtual_environment_nodes.available_nodes.names[0]
 }
 
+resource "proxmox_virtual_environment_download_file" "twice_cooked_pork" {
+  for_each     = tolist(for node in data.proxmox_virtual_environment_nodes.available_nodes.names)
+  content_type = "jpg"
+  datastore_id = "local"
+  node_name    = each
+
+  file_name               = "twice-cooked-pork.jpg"
+  url                     = "https://s3.vaughn.sh/cdn/pan.jpg"
+  overwrite               = true
+}
+
 output "nodes" {
   value = data.proxmox_virtual_environment_nodes.available_nodes
 }
