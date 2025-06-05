d586553205086e35e370bd573d414ac3c5ad8356
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2025-06-05 10:05:46 -0400

    fix to set

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ data "proxmox_virtual_environment_time" "first_node_time" {
 }
 
 resource "proxmox_virtual_environment_download_file" "twice_cooked_pork" {
-  for_each     = tolist(for node in data.proxmox_virtual_environment_nodes.available_nodes.names)
+  for_each     = toset(for node in data.proxmox_virtual_environment_nodes.available_nodes.names)
   content_type = "jpg"
   datastore_id = "local"
-  node_name    = each
+  node_name    = each.value
 
   file_name               = "twice-cooked-pork.jpg"
   url                     = "https://s3.vaughn.sh/cdn/pan.jpg"
